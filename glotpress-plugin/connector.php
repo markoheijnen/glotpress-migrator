c2197ad151177e0bb37bd9e941804654e34b63b0
Marko Heijnen <info@markoheijnen.com>
2013-02-28 22:48:37 +0100

    Configuration through defines

--- glotpress-plugin/connector.php
+++ glotpress-plugin/connector.php
@@ -1,19 +1,26 @@
 <?php
 
 class Connector {
-	private $site_url = '/wpusers/';
+	private $wordpress_url = '/';
 	private $permalink = false;
 
 	function __construct() {
-		add_action( 'after_hello', array( $this, 'show_register_link' ) );
+		if( defined( 'WORDPRESS_URL' ) ) {
+			$this->wordpress_url = WORDPRESS_URL;
+			
+			if( defined( 'WORDPRESS_PERMALINK' ) && WORDPRESS_PERMALINK )
+				$this->permalink = true;
+
+			add_action( 'after_hello', array( $this, 'show_register_link' ) );
+		}
 	}
 
 	function show_register_link() {
 		if ( ! GP::$user->logged_in() ) {
 			if( $this->permalink )
-				echo '<strong><a href="' . $this->site_url . 'register/">' . __('Register') . '</a></strong>';
+				echo '<strong><a href="' . $this->wordpress_url . 'register/">' . __('Register') . '</a></strong>';
 			else
-				echo '<strong><a href="' . $this->site_url . '?gp_action=register">' . __('Register') . '</a></strong>';
+				echo '<strong><a href="' . $this->wordpress_url . '?gp_action=register">' . __('Register') . '</a></strong>';
 		}
 	}
 }
